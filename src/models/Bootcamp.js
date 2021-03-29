4e2810f5947cf6f7e76d877577610d086e7ab8c5
Danial Farzaneh <danialf@live.com>
2021-03-29 13:14:08 +0430

    Mongoose midleware slugify

--- src/models/Bootcamp.js
+++ src/models/Bootcamp.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import slugify from 'slugify';
 
 const BootcampSchema = new mongoose.Schema({
      name: {
@@ -96,6 +97,15 @@ const BootcampSchema = new mongoose.Schema({
           type: Date,
           default: Date.now
      }
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save',function(next) {
+     this.slug = slugify(this.name, {
+          lower:true,
+          replacement: '-'
+     })
+     next();
 });
 
 const Bootcamp = mongoose.model('bootcamp',BootcampSchema);
