84098c9e6b4cd70a829d1c78b29bd40e9edc1be0
Danial Farzaneh <danialf@live.com>
2021-03-29 11:00:23 +0430

    add mongoose

--- src/server.js
+++ src/server.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({
      path: './config/config.env'
 });
 
+
 // Routes
 const bootcamps = require('./routes/bootcampRoute');
 
 const app = express();
 
-if(process.env.NODE_ENV !== "production")
-{
+if (process.env.NODE_ENV !== "production") {
      app.use(morgan('dev'));
 }
 
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+     PORT,
+     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejection
+process.on('uncaughtException', (err, promise) => {
+     console.log(`Error: ${err.message}`);
+
+     // close the server
+     server.close(() => process.exit(1));
+})
